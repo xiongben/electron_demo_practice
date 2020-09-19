a5dabce7991529d786c7ad9da013a6f19846652a
xiongben <xiongben1993@gmail.com>
2020-09-19 11:32:54 +0800

    [add] init

---
+++ main.js
@@ -0,0 +1,18 @@
+
+var electron = require('electron')
+
+var app = electron.app
+var BrowserWindow = electron.BrowserWindow
+
+var mainWindow = null
+
+
+app.on('ready',()=>{
+    mainWindow = new BrowserWindow({width:300,height:300})
+    mainWindow.loadFile('index.html')
+    mainWindow.on('close',()=>{
+        mainWindow = null
+    })
+})
+
+
